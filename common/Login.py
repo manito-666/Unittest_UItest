21aeeb5157955e332f027e0eeb9a6da6909dc6de
manito-666 <983338654@qq.com>
2019-12-11 10:23:50 +0800

    公共方法

---
+++ common/Login.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+
+class Login:
+    def login(self):
+        # 实例化浏览器
+        self.d= webdriver.Chrome()
+        # 打开登录界面
+        self.d.get("http://192.168.12.205/login")
+        # WebDriverWait(self.d,60.1).until(lambda ele:self.d.find_element_by_xpath('//*[@id="app"]/div/div[1]/form/div[1]/div/div/input'))
+        # self.d.find_element_by_xpath('//*[@id="app"]/div/div[1]/form/div[1]/div/div/input').clear()
+        # sleep(1)
+        # self.d.find_element_by_xpath('//*[@id="app"]/div/div[1]/form/div[1]/div/div/input').send_keys("admin")
+        # sleep(1)
+        # self.d.find_element_by_xpath('//*[@id="app"]/div/div[1]/form/div[2]/div/div/input').clear()
+        # sleep(1)
+        # self.d.find_element_by_xpath('//*[@id="app"]/div/div[1]/form/div[2]/div/div/input').send_keys('admin')
+        # sleep(1)
+        #点击提交登录系统
+        self.d.find_element_by_xpath('//*[@id="app"]/div/div[1]/form/div[3]/div/button').click()
+        sleep(2)
---
+++ common/img.py
@@ -0,0 +1,18 @@
+# -*-coding:utf-8 -*-
+import os
+from datetime import datetime
+from config import globalparam
+"""
+屏幕截图--出现异常
+"""
+def screenshot(driver, case_name):
+    file_path =globalparam.img_path
+    screenshotPath = os.path.join(file_path, case_name)
+    time_now = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+    screen_shot_name = "CheckPoint_NG.png"
+    screen_img = screenshotPath + '_' + time_now + '_' + screen_shot_name
+    driver.get_screenshot_as_file(screen_img)
+    m=globalparam.img_path
+    print("截图已保存在:"+m)
+    # driver.get_screenshot_as_base64(screen_img)
+    return screen_img
