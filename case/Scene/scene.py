8d1ae8d3ec81ece85c542f1e5739730dbae65665
manito-666 <983338654@qq.com>
2019-12-11 10:21:05 +0800

    场景

---
+++ case/Scene/scene.py
@@ -0,0 +1,120 @@
+# coding=utf-8
+from common.Login import Login
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from common.img import screenshot
+class CJ():
+    def __init__(self):
+        L = Login()
+        L.login()
+        self.d = L.d
+        self.d.maximize_window()
+        # 点击数据模版管理
+        WebDriverWait(self.d, 60, 1).until(lambda ele: self.d.find_element_by_css_selector('#index > div.second-container > div:nth-child(1) > div > div > ul > li:nth-child(3) > div'))
+        self.d.find_element_by_css_selector('#index > div.second-container > div:nth-child(1) > div > div > ul > li:nth-child(3) > div').click()
+        sleep(2)
+        # 点击场景
+        self.d.find_element_by_css_selector("#index > div.second-container > div:nth-child(1) > div > div > ul > li:nth-child(3) > ul > li:nth-child(3)").click()
+
+    def add(self,name,beizhu):
+        # 添加场景
+        WebDriverWait(self.d,10,1).until(lambda ele:self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[2]/span'))
+        self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[2]/span').click()
+        # 输入场景名字
+        self.d.find_element_by_xpath( '//*[@id="index"]/div[2]/div[2]/div/div[2]/div[4]/div/div[2]/form/div[1]/div/div/input').send_keys(name)
+        sleep(1)
+        # 选择行业
+        self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[4]/div/div[2]/form/div[2]/div/div/div[1]/span/span/i').click()
+        sleep(1)
+        self.d.find_element_by_xpath('/html/body/div[3]/div[1]/div[1]/ul/li[2]').click()
+        sleep(1)
+        self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[4]/div/div[2]/form/div[3]/div/div/textarea').send_keys(beizhu)
+        # 点击提交
+        try:
+            self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[4]/div/div[3]/div/button[1]').click()
+            WebDriverWait(self.d, 10, 1).until(lambda ele: self.d.find_element_by_css_selector('body > div.el-message.el-message--success > p'))
+            t = self.d.find_element_by_css_selector('body > div.el-message.el-message--success > p').text
+            self.d.close()
+            return t
+        except Exception as msg:
+            print(u"异常原因:场景名不能重复")
+            screenshot(self.d,'changjing')
+            raise msg
+
+    def alter(self,name):
+        # 点击编辑
+        WebDriverWait(self.d,10,1).until(lambda ele:self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[3]/div[1]/div[3]/table/tbody/tr[10]/td[5]/div/button[1]/span'))
+        self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[3]/div[1]/div[3]/table/tbody/tr[10]/td[5]/div/button[1]/span').click()
+        sleep(2)
+        # 修改场景名称
+        self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[4]/div/div[2]/form/div[1]/div/div/input').clear()
+        sleep(1)
+        self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[4]/div/div[2]/form/div[1]/div/div/input').send_keys(name)
+        sleep(1)
+        # 修改备注
+        self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[4]/div/div[2]/form/div[3]/div/div/textarea').send_keys("自动化测试")
+        sleep(1)
+        # 点击提交
+        try:
+            self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[4]/div/div[3]/div/button[1]').click()
+            WebDriverWait(self.d, 10, 1).until(lambda ele: self.d.find_element_by_css_selector('body > div.el-message.el-message--success'))
+            t = self.d.find_element_by_css_selector('body > div.el-message.el-message--success').text
+            self.d.close()
+            return t
+        except Exception as msg:
+            screenshot(self.d, 'changjing')
+            raise
+
+    def select_cj(self):
+        # 输入场景名称进行查找
+        WebDriverWait(self.d,10,1).until(lambda ele:self.d.find_element_by_css_selector("#index > div.second-container > div.main-wrapper > div > div:nth-child(2) > div.common-search > form > div:nth-child(1) > div > div > input"))
+        self.d.find_element_by_css_selector("#index > div.second-container > div.main-wrapper > div > div:nth-child(2) > div.common-search > form > div:nth-child(1) > div > div > input").send_keys("手术")
+        sleep(1)
+        try:
+            self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[1]/form/div[3]/div/button[2]').click()
+            sleep(1)
+            m = self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[3]/div[1]/div[3]/table/tbody/tr[1]/td[5]/div/button[1]').text
+            self.d.close()
+            return m
+        except Exception as msg:
+            print(u"异常原因:未找到该元素")
+            screenshot(self.d,'changjing')
+            raise
+
+    def select_hy(self):
+        # 选择行业进行查询
+        self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[1]/form/div[2]/div/div/div/span/span/i').click()
+        sleep(2)
+        self.d.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/ul/li[1]').click()
+        sleep(1)
+        try:
+            self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[1]/form/div[3]/div/button[2]').click()
+            sleep(1)
+            m = self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[3]/div[1]/div[3]/table/tbody/tr[1]/td[5]/div/button[1]').text
+            self.d.close()
+            return m
+        except Exception as msg:
+            print(u"异常原因:未找到该元素")
+            screenshot(self.d, 'changjing')
+            raise
+
+    def deleter(self):
+        WebDriverWait(self.d, 60, 1).until(lambda ele: self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[3]/div[2]/ul/li[2]'))
+        self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[3]/div[2]/ul/li[2]').click()
+        sleep(2)
+        WebDriverWait(self.d, 60, 1).until(lambda ele:self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[3]/div[1]/div[3]/table/tbody/tr[1]/td[5]/div/button[2]/span'))
+        self.d.find_element_by_xpath('//*[@id="index"]/div[2]/div[2]/div/div[2]/div[3]/div[1]/div[3]/table/tbody/tr[1]/td[5]/div/button[2]/span').click()
+        sleep(1)
+        try:
+            self.d.find_element_by_xpath('/html/body/div[2]/div/div[3]/button[2]').click()
+            WebDriverWait(self.d, 60, 1).until(lambda ele: self.d.find_element_by_css_selector('body > div.el-message.el-message--success'))
+            m = self.d.find_element_by_css_selector("body > div.el-message.el-message--success").text
+            self.d.close()
+            return m
+        except Exception as msg:
+            print(u"异常原因:场景正在被使用无法删除")
+            screenshot(self.d, 'changjing')
+            raise
+
+if __name__ == '__main__':
+    CJ().add("手术","11")
