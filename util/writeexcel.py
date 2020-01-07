7894408cb9346ab7754e5ba8471a0e1855cd7641
manito-666 <983338654@qq.com>
2020-01-07 16:38:08 +0800

    数据读写

--- util/writeexcel.py
+++ util/writeexcel.py
@@ -4,6 +4,9 @@ Path = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(Path)[0]
 sys.path.append(rootPath)
 from openpyxl import load_workbook
+Path = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(Path)[0]
+sys.path.append(rootPath)
 import openpyxl,random
 Path = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(Path)[0]
